2d00068ad092bed169275b69bb54aca35d669fae
Hem Shrestha <hereshem@gmail.com>
2022-02-18 20:36:20 +0545

    webshell initial

---
+++ controllers/terminal.go
@@ -0,0 +1,18 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+type TerminalController struct {
+	beego.Controller
+}
+
+func (self *TerminalController) Get() {
+	self.Data["path"] = self.GetString("path")
+	self.Data["context"] = self.GetString("context")
+	self.Data["namespace"] = self.GetString("namespace")
+	self.Data["pod"] = self.GetString("pod")
+	self.Data["container"] = self.GetString("container")
+	self.TplName = "terminal.html"
+}
